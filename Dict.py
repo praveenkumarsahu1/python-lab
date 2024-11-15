e7a83322f46b0ee4f5175adfc37308ea363c3e6a
Praveen Kumar Sahu <praveenkumarsahu17@gmail.com>
2024-11-15 20:20:10 +0530

    Create Dict.py

---
+++ Dict.py
@@ -0,0 +1,7 @@
+DICITIONARIES ARE WRITTEN IN CURLY BRACES:
+Example
+games={1:'hockey',2:'basketball',3:'volleyball',4:'cricket'}
+print(games)
+games
+best_smartphones={1:'samsung',2:'redmi',3:'motorola',4:'lenovo',5:'apple'}
+best_smartphones
